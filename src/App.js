88020ef2ac668fd2877b45ba44afecdedd0e604a
gaus <gausiyaansa@jn>
2023-02-26 19:26:20 +0530

    Refactoring App

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import Navbar from './components/Navbar';
 import News from './components/News';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 export default class App extends Component {
+  pageSize = 10;
   render() {
     return (
       <div>
         <Router>
         <Navbar/>
         <Routes>
-          <Route exact path="/" element={<News key="sports" pageSize={6} country="us" category="sports"/>}/>
-          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={6} country="us" category="entertainment"/>}/>
-          <Route exact path="/business" element={<News key="business" pageSize={6} country="us" category="business"/>}/>
-          <Route exact path="/general" element={<News key="general" pageSize={6} country="us" category="general"/>}/>
-          <Route exact path="/health" element={<News key="health" pageSize={6} country="us" category="health"/>}/>
-          <Route exact path="/science" element={<News key="science" pageSize={6} country="us" category="science"/>}/>
-          <Route exact path="/technology" element={<News key="technology" pageSize={6} country="us" category="technology"/>}/>
+          <Route exact path="/" element={<News key="sports" pageSize={this.pageSize} country="us" category="sports"/>}/>
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/>}/>
+          <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="us" category="business"/>}/>
+          <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="us" category="general"/>}/>
+          <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="us" category="health"/>}/>
+          <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="us" category="science"/>}/>
+          <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="us" category="technology"/>}/>
         </Routes>
         </Router>
       </div>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -8,7 +8,7 @@ export class Newsitem extends Component {
         <div className="card border-dark">
             <img src={!imageUrl?"https://source.unsplash.com/1200x630/?us":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title} <span class="badge rounded-pill text-bg-warning">{source}</span></h5>
+                <h5 className="card-title">{title} <span className="badge rounded-pill text-bg-warning">{source}</span></h5>
                 <p className="card-text">{description}...</p>
                 <p className="card-text"><small className="text-muted">By {!author?"Unknown": author} on {new Date(date).toUTCString()}</small></p>
                 <a href={url} rel="noreferrer" target="_blank" className="btn btn-primary">Read More</a>
