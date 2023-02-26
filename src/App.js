c9fd90575eb94cddfcb69005bec85879200a280c
gaus <gausiyaansa@jn>
2023-02-26 19:56:13 +0530

    Refactoring App

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ export default class App extends Component {
         <Router>
         <Navbar/>
         <Routes>
-          <Route exact path="/" element={<News key="sports" pageSize={this.pageSize} country="us" category="sports"/>}/>
+          <Route exact path="/" element={<News key="sports" pageSize={this.pageSize} country="us" category="general"/>}/>
           <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/>}/>
           <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="us" category="business"/>}/>
           <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="us" category="general"/>}/>
           <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="us" category="health"/>}/>
           <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="us" category="science"/>}/>
+          <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="us" category="sports"/>}/>
           <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="us" category="technology"/>}/>
         </Routes>
         </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -14,14 +14,18 @@ export class News extends Component {
    pageSize: PropTypes.number,
    category: PropTypes.string,
   }
+  capitlizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page:1
     }
+    document.title = `${this.capitlizeFirstLetter(this.props.category)} - NewsApp`;
   }
   async updateNews(){
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c9226c17120d49ea971e9697b6118820&page=${this.state.page}&pageSize=${this.props.pageSize}`;
@@ -50,41 +54,18 @@ export class News extends Component {
   }
 
   handlePreClick = async ()=>{
-    // console.log("Previous");
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c9226c17120d49ea971e9697b6118820&page=${this.state.page - 1}&pagesize=${this.props.pageSize}`;
-    // this.setState({loading: true});
-    // let data = await fetch(url);
-    // let parsedData = await data.json()
-    // console.log(parsedData);
-    // this.setState({
-    //   page: this.state.page + 1,
-    //   articles: parsedData.articles,
-    //   loading: false
-    // })
     this.setState({page: this.state.page - 1});
     this.updateNews();
   }
   handleNextClick = async ()=>{
-    console.log("Next");
-    // if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-    //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c9226c17120d49ea971e9697b6118820&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    //   this.setState({loading: true});
-    //   let data = await fetch(url);
-    //   let parsedData = await data.json()
-    //   this.setState({
-    //     page: this.state.page + 1,
-    //     articles: parsedData.articles,
-    //     loading: false
-    //   })
-    // }
-    this.setState({page: this.state.page + 1});
+    setState({page: this.state.page + 1});
     this.updateNews();
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h1>NewsApp - United States Top Headline</h1>
+        <h1>NewsApp - United States Top {this.capitlizeFirstLetter(this.props.category)} Headline</h1>
         {this.state.loading && <Spinner/>}
         <div className="row my-5">
         {this.state.articles.map((element)=>{
